9dfeb1d905260c68647cc8d080063f109ee648a3
Fedulov Danil <danya7777kg@gmail.com>
2025-12-17 22:09:57 +0600

    Написал логику для добавления фильмов и подклюил библиотеку react-toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,42 @@
 import './App.css';
 import Form from './components/Form/Form';
+import {useState} from 'react';
+import type {ICinema} from './types';
+import {toast, ToastContainer} from 'react-toastify';
+
 
 const App = () => {
 
+    const [cinema, setCinema] = useState<ICinema[]>([]);
 
 
     const onSubmitAddCinema = (movieName: string) => {
-        console.log(movieName);
-    }
+
+        const movieCopy = cinema.find(movie => movie.name === movieName);
+
+
+        if (!movieName.trim()) {
+            toast.error('Вы ничего не ввели!');
+        }
+
+         if (movieCopy) {
+            toast.error('Этот фильм уже добавлен в список!');
+        }
+
+        setCinema(prevState =>  [
+            ...prevState,
+            {
+                id: new Date().toISOString(),
+                name: movieName,
+            }
+        ]);
+    };
+
 
     return (
             <>
-                 <div className='container my-4 p-4 border border-2  border-gray rounded'>
+                 <ToastContainer/>
+                 <div className='container my-4 p-4 border border-2  border-primary rounded'>
                        <Form onSubmit={onSubmitAddCinema} />
                  </div>
             </>
